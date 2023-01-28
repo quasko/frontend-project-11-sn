1c1c423e39a333dd86d9e21e0d1b5ba7adb9af13
Nick <smuskin2016@yandex.ru>
2023-01-28 17:45:08 +0300

    finished third step

--- src/view.js
+++ src/view.js
@@ -1,20 +1,43 @@
 import onChange from 'on-change';
+import i18next from 'i18next';
 import * as yup from 'yup';
+
+const i18nextInstance = i18next.createInstance();
+
+i18nextInstance.init({
+    lng: 'ru', 
+    debug: true,
+    resources: {
+        ru: {
+            translation: {
+                statusMessage: {
+                    neutral: '',
+                    valid: 'RSS успешно загружен',
+                    invalid: 'Ссылка должна быть валидным URL',
+                    duplicate: 'RSS уже существует'
+                },
+            }
+          }
+        }
+      });
+
 const schema = yup.object({
     link: yup.string()
     .url()
     .required()
 });
+
 const handleProcessState = (submitButton, processState) => {
+    console.log(processState)
     switch(processState) {
         case 'sent':
-            submitButton.disabled = true;
-            break;
-        case 'sending':
             submitButton.disabled = false;
             break;
-        case 'error':
+        case 'sending':
             submitButton.disabled = true;
+            break;
+        case 'error':
+            submitButton.disabled = false;
         case 'filling':
             submitButton.disabled = false;
             break;
@@ -27,22 +50,26 @@ const app = () => {
     const validation = (field) => {
             schema.validate({link: field}, {abortEarly: true})
             .then(() => {
+                state.form.data.push(elements.input.value);
                 watchedState.form.dataState = 'valid';
-                console.log('Congrats!')
-                console.log(watchedState.form.dataState)
+                state.form.processState = 'sent';
+                handleProcessState(elements.submitButton, state.form.processState);
             })
-            .catch (e => {
-                watchedState.form.dataState = 'invalid'
-                console.log(watchedState.form.dataState)
-                console.log(e)
+            .catch(e => {
+                state.form.processState = 'error';
+                watchedState.form.dataState = 'invalid';
+                handleProcessState(elements.submitButton, state.form.processState);
+                
             }) 
     };
+
     const elements = {
         form: document.querySelector('.rss-form'),
         statusMassage: document.querySelector('.feedback'),
         input: document.querySelector('#url-input'),
         submitButton: document.querySelector('.btn-primary'),
     };
+
     const state = {
         form: {
             processState: 'filling',
@@ -52,35 +79,44 @@ const app = () => {
             field: ''
         }
     };
-    const render = (statusMassage, state) => {
-        console.log(statusMassage)
-        console.log(state)
-        const alertMassage = statusMassage;
-        console.log(elements.form)
-        switch(state) {
+
+    const render = (status) => (path, value) =>  {
+        switch(value) {
             case 'neutral':
-                alertMassage.classList.remove('text-sucess', 'text-danger')
-                alertMassage.innerHTML = '';
+                status.classList.remove('text-sucess', 'text-danger');
+                status.textContent = i18nextInstance.t('statusMessage.neutral');
                 break;
             case 'invalid':
-                alertMassage.classList.remove('text-sucess', 'text-danger');
-                alertMassage.classList.add('text-danger');
-                alertMassage.innerHTML = 'Ссылка должна быть валидным URL';
+                status.classList.remove('text-sucess', 'text-danger');
+                status.classList.add('text-danger');
+                status.textContent = i18nextInstance.t('statusMessage.invalid');
+                elements.input.value = '';
+                elements.input.focus()
+
                 break;
             case 'valid':
-                alertMassage.classList.remove('text-sucess', 'text-danger');
-                alertMassage.classList.add('text-sucess');
-                alertMassage.innerHTML = 'RSS успешно загружен';
+                status.classList.remove('text-sucess', 'text-danger');
+                status.classList.add('text-sucess');
+                status.textContent = i18nextInstance.t('statusMessage.valid');
+                elements.input.value = '';
+                elements.input.focus() 
                 break;
-            case 'dublicate':
-                alertMassage.classList.remove('text-sucess', 'text-danger');
-                alertMassage.classList.add('text-danger');
-                alertMassage.innerHTML = 'RSS уже существует';
+            case 'duplicate':
+                status.classList.remove('text-sucess', 'text-danger');
+                status.classList.add('text-danger');
+                status.textContent = i18nextInstance.t('statusMessage.duplicate');
+                elements.input.value = '';
+                elements.input.focus() 
                 break;
+            default:
+                throw new Error(`Unknown data state: ${value}`);
         }
     };
-    const watchedState = onChange(state, render(elements.statusMassage, state.form.dataState));
-    const form = elements.form
+
+    const watchedState = onChange(state, render(elements.statusMassage));
+
+    const form = elements.form;
+
     form.addEventListener('submit', (e) => {
         e.preventDefault();
         state.form.processState = 'sending';
@@ -88,11 +124,12 @@ const app = () => {
         handleProcessState(elements.submitButton, state.form.processState);
 
             if (state.form.data.includes(elements.input.value)) {
-                watchedState.form.dataState = 'dublicate';
+                watchedState.form.dataState = 'duplicate';
+                state.form.processState = 'error';
+                handleProcessState(elements.submitButton, state.form.processState);
             } else {
-                state.form.data.push(elements.input.value)
-                validation(elements.input.value)
-            } 
+                validation(elements.input.value);
+            };  
     });
 
 };
